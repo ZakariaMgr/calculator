68d4786b2362f53ce589dcfb7c3d28923c51fc14
ZakariaMgr <mgari.zakaria05@gmail.com>
2023-12-07 20:51:37 -0500

    Ajouter la logique de l’addition, controller et test unitaire

---
+++ src/test/java/com/mgari/calculator/CalculatorApplicationTest.java
@@ -0,0 +1,15 @@
+package com.mgari.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- src/test/java/com/mgari/calculator/CalculatorTest.java
+++ src/test/java/com/mgari/calculator/CalculatorTest.java
@@ -1,10 +1,14 @@
 package com.mgari.calculator;
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
-	private Calculator calculator = new Calculator();
-	@Test
-	public void testSum() {
-		assertEquals(8, calculator.sum(3, 5));
-	}
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum() {
+        assertEquals(8, calculator.sum(3, 5));
+    }
 }
+
