808eba435b7b97b25adb5faefbf6d9c641f0b65f
ZakariaMgr <mgari.zakaria05@gmail.com>
2023-12-21 16:04:30 -0500

    add nouveau test

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-test $(curl localhost:8765/sum?a=80\&b=20) -eq 100
+
+# Exécutez la commande curl et stockez la réponse dans une variable
+response=$(curl -s localhost:8765/sum?a=80\&b=20)
+
+# Vérifiez si la réponse est vide
+if [ -z "$response" ]; then
+  echo "Erreur: La réponse du serveur est vide."
+  exit 1
+fi
+
+# Vérifiez si la réponse est égale à 100
+if [ "$response" -eq 100 ]; then
+  echo "Test réussi : La somme est égale à 100."
+else
+  echo "Échec du test : La somme n'est pas égale à 100. Réponse reçue : $response"
+  exit 2
+fi
+
